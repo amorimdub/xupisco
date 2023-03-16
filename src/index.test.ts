7ca9667202405af036e6e7fb3a44e550cf4cbb2e
Felipe Amorim <amorimdub@gmail.com>
2023-03-16 12:35:55 +0000

    add simple test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,42 @@
-import { describe, expect, it } from "vitest";
+import dotenv from "dotenv";
+dotenv.config();
 
-describe("my test", () => {
-  it("should pass", () => {
-    expect(1).toBe(1);
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { createClient } from "redis";
+
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+describe("REDIS", async () => {
+  beforeAll(async () => {
+    await client.connect();
+  });
+  beforeEach(async () => {
+    await client.flushDb();
+  });
+  describe("JSON", async () => {
+    it("SIMPLE SET / GET", async () => {
+      const course = {
+        course: "ru204",
+        exercise: "1.2.1",
+        status: "complete",
+      };
+
+      await client.json.set(
+        "ru204:exercise:1.2.1",
+        ".",
+        JSON.stringify(course)
+      );
+      const result = await client.json.get("ru204:exercise:1.2.1", {
+        path: ".",
+      });
+      const parsed = JSON.parse(result as string);
+
+      expect(course.course).toBe(parsed.course);
+    });
+
+    afterAll(async () => {
+      await client.disconnect();
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
-import { Server, IncomingMessage, ServerResponse } from "http";
-import { createClient } from "redis";
-
-const client = createClient({
-  url: process.env.REDIS_URL,
-});
-client.on("error", (err) => console.log("Redis Client Error", err));
+import Fastify, { FastifyInstance } from "fastify";
 
 const start = async () => {
   const server: FastifyInstance = Fastify({ logger: true });
   server.get("/ping", async (request, reply) => {
-    const value = await client.get("key");
-    return { pong: "it worked!", value };
+    return { pong: "it worked!" };
   });
   try {
-    await client.connect();
-
     await server.listen({ port: 3000 });
-
     const address = server.server.address();
     const port = typeof address === "string" ? address : address?.port;
   } catch (err) {
